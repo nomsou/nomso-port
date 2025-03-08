7169478fc333fa210d5640524ba9f43edbcaf01a
Nomso <nomsounaogu@gmail.com>
2025-03-08 14:01:10 +0100

    reduced quantity of particles and duration when a user gets on the site

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,9 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={5000}
+        quantity={1000}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-2000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text ">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text ">
         NOMSO
       </h1>
 
