e9bb1ae4a166d66b1c08caad86435792e54fc8bf
Nomso <nomsounaogu@gmail.com>
2025-03-08 12:58:14 +0100

    added and commented out alternate text styling

--- app/page.tsx
+++ app/page.tsx
@@ -31,9 +31,17 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={5000}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-4000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text ">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-2000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text ">
         NOMSO
       </h1>
+
+      {/* Alternate text styling */}
+      {/* <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        onare
+      </h1>
+      <h5 className="z-10 text-3xl text-transparent duration-1000 bg-zinc-900 cursor-default text-edge-outline animate-title font-display sm:text-6lg md:text-9lg whitespace-nowrap bg-clip-text ">
+        development
+      </h5> */}
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
