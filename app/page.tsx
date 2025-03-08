eca207ba7ab212a18f14ef8ca39d540ba395bbc9
Nomso <nomsounaogu@gmail.com>
2025-03-08 11:54:39 +0100

    Resume Link: Open in a new tab on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  // { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
-  {
-    name: "Resume",
-    href: "/resume.pdf",
-    target: "_blank",
-    rel: "noopener noreferrer",
-  },
   { name: "Youtube", href: "https://www.youtube.com/nomso" },
 ];
 
@@ -28,18 +21,27 @@ export default function Home() {
               {item.name}
             </Link>
           ))}
+          {/* Resume Link: Opens in a new tab */}
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-zinc-300 sm:text-zinc-500 text-lg duration-500 hover:text-zinc-300 font-semibold"
+          >
+            Resume
+          </a>
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={5000}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-4000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text ">
+
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-4000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-7xl md:text-8xl whitespace-nowrap bg-clip-text">
         NOMSO
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-zinc-300 sm:text-zinc-500 mx-6 font-semibold">
           I'm currently building{" "}
